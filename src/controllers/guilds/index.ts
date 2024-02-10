4072e65e302432c79f965291bf5250669dc10baf
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-10 09:08:11 +0530

    updated

--- src/controllers/guilds/index.ts
+++ src/controllers/guilds/index.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
-import { getBotGuildsService } from "../../services/guilds";
+import {
+  getBotGuildsService,
+  getUserGuildsService,
+} from "../../services/guilds";
+import { User } from "../../data-base/schemas/user";
 
 export async function getGuildController(req: Request, res: Response) {
+  const user = req.user as User;
   try {
-    const { data } = await getBotGuildsService();
-    res.send(data);
+    const { data: botGuilds } = await getBotGuildsService();
+    const { data: userGuilds } = await getUserGuildsService(user.id);
+    res.send({
+      botGuilds,
+      userGuilds,
+    });
   } catch (error) {
     console.log(error);
     res.status(400).send("Error");
